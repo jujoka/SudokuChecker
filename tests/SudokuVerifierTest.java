1c6c7ec6a36fc1b738a390f4e9c80b093b1ca407
juhok <juho.kantakoski@hotmail.com>
2020-11-27 10:29:25 +0200

    Added unit tests

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -8,6 +8,7 @@ public class SudokuVerifierTest {
 	
 // A correct Sudoku string: 417369825632158947958724316825437169791586432346912758289643571573291684164875293
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
+	
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 SudokuVerifier v = new SudokuVerifier();
@@ -25,5 +26,75 @@ SudokuVerifier v = new SudokuVerifier();
 		
 	}
 	
+	@Test
+	public void testLongString()
+	{
+		String toolong = "4173698256321589479587243168254371697915864323469127582896435715732916841648752931";
+		int a = v.verify(toolong);
+		assertEquals("string > 81 characters", a, -1);
+	}
 	
+	@Test
+	public void testShortString()
+	{
+		String tooshort = "41736982563215894795872431682543716979158643234691275828964357157329168416487529";
+		int a = v.verify(tooshort);
+		assertEquals("string < 81 characters", a, -1);
+	}
+	
+	@Test
+	public void testNormalLengthString()
+	{
+		String normal = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(normal);
+		assertEquals("normal length string", a, 0);
+	}
+	
+	@Test
+	public void testNonNumericalValue()
+	{
+		String nonnumeric = "abc369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(nonnumeric);
+		assertEquals("nonnumeric value", a, 1);
+	}
+	
+	@Test
+	public void testDecimalValue()
+	{
+		String decimal = "0.117369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(decimal);
+		assertEquals("decimal value", a, -1);
+	}
+	
+	@Test
+	public void testNegativeValue()
+	{
+		String negative = "-417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(negative);
+		assertEquals("negative value", a, -1);
+	}
+	
+	@Test
+	public void testDuplicateInRow()
+	{
+		String row = "417367825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(row);
+		assertEquals("duplicate in row", a, -2);
+	}
+	
+	@Test
+	public void testDuplicateInColumn()
+	{
+		String column = "417369825432158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(column);
+		assertEquals("duplicate in column", a, -2);
+	}
+	
+	@Test
+	public void testDuplicateInGrid()
+	{
+		String grid = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(grid);
+		assertEquals("duplicate in grid", a, -2);
+	}
 }
